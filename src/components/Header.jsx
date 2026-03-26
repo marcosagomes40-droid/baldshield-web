0e77f0ddac1a342e25f1012ece061d52e66143e1
marcosagomes40-droid <marcos.agomes40@gmail.com>
2026-03-26 18:37:20 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,5 @@ const navLinks = [
   { name: 'Início', path: '/' },
   { name: 'Produtos', path: '/products' },
   { name: 'Benefícios', path: '/benefits' },
-  { name: 'Por que existimos', path: '/about' },
   { name: 'Contato', path: '/contact' },
 ];
